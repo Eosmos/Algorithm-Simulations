a87d0837b53541a87c7b0b6a19eb3c1f1c63cdf2
sourav paria <souravparia9695@live.com>
2025-03-30 12:19:07 +0530

    decision-tree added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LinearRegressionComponent } from "./linear-regression/linear-regression.component";
+import { DecisionTreeComponent } from "./decision-tree/decision-tree.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LinearRegressionComponent],
+  imports: [RouterOutlet, LinearRegressionComponent, DecisionTreeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
