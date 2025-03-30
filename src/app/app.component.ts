dc2d1c1b45a09d7c171047b7c16b8db400ccaa1f
sourav paria <souravparia9695@live.com>
2025-03-30 13:30:46 +0530

    random-forest-simulation added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { LinearRegressionComponent } from "./linear-regression/linear-regression.component";
 import { DecisionTreeComponent } from "./decision-tree/decision-tree.component";
 import { LogisticRegressionSimulationComponent } from "./logistic-regression-simulation/logistic-regression-simulation.component";
+import { RandomForestSimulationComponent } from "./random-forest-simulation/random-forest-simulation.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LinearRegressionComponent, DecisionTreeComponent, LogisticRegressionSimulationComponent],
+  imports: [RouterOutlet, LinearRegressionComponent, DecisionTreeComponent, LogisticRegressionSimulationComponent, RandomForestSimulationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
